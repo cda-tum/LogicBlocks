710a9bf2e31ce32d8c74085b721b23b2d3bdff07
Sarah Schneider <mail@sarah-schneider.at>
2022-03-02 10:16:33 +0100

    svae

--- src/App/app.cpp
+++ src/App/app.cpp
@@ -8,25 +8,26 @@
 #include "LogicTerm/LogicTerm.hpp"
 #include "utils/util.hpp"
 
+using namespace logicbase;
 int main(int argc, char *argv[]) {
   namespace po = boost::program_options;
   po::options_description desc("Allowed options");
 
   z3logic::Z3LogicBlock z3logic(true);
 
-  logicbase::LogicTerm a =
-      z3logic.makeVariable("a", logicbase::CType::BITVECTOR, 5);
-  logicbase::LogicTerm b =
-      z3logic.makeVariable("b", logicbase::CType::BITVECTOR, 5);
-  logicbase::LogicTerm c =
-      z3logic.makeVariable("c", logicbase::CType::BITVECTOR, 5);
-  z3logic.assertFormula((a && b) == c);
-  z3logic.assertFormula(c == logicbase::LogicTerm(10, 5));
-  z3logic.assertFormula(a != logicbase::LogicTerm(10, 5));
+  LogicTerm a = z3logic.makeVariable("a", CType::BITVECTOR, 5);
+  LogicTerm b = z3logic.makeVariable("b", CType::BITVECTOR, 5);
+  LogicTerm c = z3logic.makeVariable("c", CType::BITVECTOR, 5);
+  LogicTerm d = z3logic.makeVariable("d", CType::BITVECTOR, 5);
+  LogicTerm e = z3logic.makeVariable("e", CType::BITVECTOR, 5);
+  LogicTerm f = z3logic.makeVariable("f", CType::BITVECTOR, 5);
+  LogicTerm g = z3logic.makeVariable("g", CType::BITVECTOR, 5);
+  LogicTerm changes = a & b;
+
   z3logic.dumpAll(std::cout);
   z3logic.produceInstance();
   z3logic.dumpZ3State(std::cout);
-  if (z3logic.solve() == logicbase::Result::SAT) {
+  if (z3logic.solve() == Result::SAT) {
     std::cout << "SAT" << std::endl;
     std::cout << "a: ";
     z3logic.getModel()->getValue(a, &z3logic).print(std::cout);
