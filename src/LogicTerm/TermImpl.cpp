f3def16046979b71e86eca4ece73a0bdba85d2d3
Sarah Schneider <sarah.schneider@jku.at>
2022-10-05 11:49:32 +0200

    Fixed code formatting

--- src/LogicTerm/TermImpl.cpp
+++ src/LogicTerm/TermImpl.cpp
@@ -240,9 +240,9 @@ bool TermImpl::deepEquals(const TermImpl& other) const {
     if (getNodes().size() != other.getNodes().size()) {
         return false;
     }
-//    if (getID() != other.getID()) {
-//        return false;
-//    }
+    //    if (getID() != other.getID()) {
+    //        return false;
+    //    }
     if (getCType() != other.getCType()) {
         return false;
     }
