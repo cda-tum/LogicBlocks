6a5e7264ed77873a649f59483d907453606a322c
Sarah Schneider <sarah.schneider@jku.at>
2022-12-01 15:49:23 +0100

    Adding OptimathLogicBlock

---
+++ include/LogicBlock/OptimathLogicBlock.hpp
@@ -0,0 +1,18 @@
+
+
+#ifndef LOGICBLOCKS_OPTIMATHLOGICBLOCK_HPP
+#define LOGICBLOCKS_OPTIMATHLOGICBLOCK_HPP
+#include "LogicBlock.hpp"
+#include "../extern/optimathsat/include/optimathsat.h"
+
+namespace optimathlogic {
+
+    using namespace logicbase;
+
+    class OptimathLogicBlock : public LogicBlock {
+
+
+    };
+}
+
+#endif //LOGICBLOCKS_OPTIMATHLOGICBLOCK_HPP
